ff9ea52fed4d52b6be39f493498169aba44fef6f
studioOwol <from.owol@gmail.com>
2023-04-27 16:44:48 +0900

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Nahyun on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
